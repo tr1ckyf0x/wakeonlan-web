48e9cee9a90f7fc1240cad897e82d4c337e189b6
Vladislav Lisyanskiy <vladislav.lisyanskiy@gmail.com>
2021-09-20 21:05:50 +0300

    Added header, updated styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 import SupportForm from "./SupportForm";
+import SupportFormHeader from './SupportFormHeader';
 
 function App() {
   return (
-      // <div className="App-container">
+    <div className="App-container">
+      <SupportFormHeader />
       <SupportForm />
-      // </div>
+    </div>
   );
 }
 
