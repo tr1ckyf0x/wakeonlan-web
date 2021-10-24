c3a4cce19767dfb83d4c3117a4a6aa06d3efadd3
Evgeny Guzhikhin <gpont97@gmail.com>
2021-10-24 17:55:03 +0300

    add privacy policy component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import './App.css';
+import { PrivacyPolicy } from './PrivacyPolicy';
 import SupportForm from "./SupportForm";
 import SupportFormHeader from './SupportFormHeader';
 
-function App() {
-  return (
-    <div className="App-container">
-      <SupportFormHeader />
-      <SupportForm />
-    </div>
-  );
-}
+const App: React.FC = () => (
+  <div className="App-container">
+    <SupportFormHeader />
+    <SupportForm />
+    <PrivacyPolicy />
+  </div>
+);
 
 export default App;
